297b06c6eaa0db8ec39acd94bb114a6715257207
gaurav <gaurav.ranganath@gmail.com>
2022-11-15 21:52:18 -0500

    added required login

--- project/app.py
+++ project/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-
+from functools import wraps
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -25,6 +25,15 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -71,6 +80,7 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
@@ -82,6 +92,15 @@ def delete_entry(post_id):
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
     return jsonify(result)
+
+
+@app.route('/search/', methods=['GET'])
+def search():
+    query = request.args.get("query")
+    entries = db.session.query(models.Post)
+    if query:
+        return render_template('search.html', entries=entries, query=query)
+    return render_template('search.html')
 
 
 if __name__ == "__main__":
